8e1d8c64603ce3cc18d0d6c97df25e4f5304b587
Jacky Chan <jc7311@rit.edu>
2024-02-15 20:54:43 -0500

    Updated interface

--- ufund-api/src/main/java/com/ufund/persistence/VillainDAO.java
+++ ufund-api/src/main/java/com/ufund/persistence/VillainDAO.java
@@ -1,5 +1,23 @@
 package com.ufund.persistence;
 
-public class VillainDAO {
-    
+import java.io.IOException;
+
+//where this come from ? import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Io;
+
+import com.ufund.model.VillainSchemeNeed;
+
+// INTERFACE 
+public interface VillainDAO {
+
+    VillainSchemeNeed[] getSchemes() throws IOException;
+
+    VillainSchemeNeed[] findSchemes(String containsScheme) throws IOException;
+
+    VillainSchemeNeed getScheme(int id) throws IOException;
+
+    VillainSchemeNeed createScheme(VillainSchemeNeed scheme) throws IOException;
+
+    VillainSchemeNeed updateScheme(VillainSchemeNeed scheme) throws IOException;
+
+    boolean deleteScheme(int id) throws IOException;
 }
