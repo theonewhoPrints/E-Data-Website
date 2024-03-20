4f2fa2f48e7e8320c1096a991205ec935a970ee8
ias2858 <131394537+theonewhoPrints@users.noreply.github.com>
2024-03-20 15:18:01 -0400

    Checkout button visible
    Checkout out button displayed

--- ufund-ui/src/app/app.module.ts
+++ ufund-ui/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartComponent } from './cart/cart.component';
 // log in
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { ProfileComponent } from './profile/profile.component';
     CartComponent,
     LoginComponent,
     ProfileComponent,
+    CheckoutComponent,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
